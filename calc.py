ac1e6720a31747706076767705c7ad30035ff39f
thegreatn <nikitos43rus@gmail.com>
2018-06-06 14:39:13 +0300

    Fix typos
    integer missing in "age' and do we really need double quotes?

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
-print("Welcome to Noob-Age-Calculator 1.0!")
-name  = input("Enter your name: ")
-age = input("Enter your age: ")
+print('Welcome to Noob-Age-Calculator 1.0!')
+name  = input('Enter your name: ')
+age = int(input("Enter your age: '))
 if age < 1:
     print('You are not human!')
     quit()
@@ -9,9 +9,9 @@ minutes = int(age) * 365 * 24 * 60
 hours = int(age) * 365 * 24
 weeks  = int(age) * 12 * 4
 months = int(age) * 12
-print("Hi, " + str(name) + ", there is your results:")
-print("Your age in seconds: " + str(seconds) + " seconds")
-print("Your age in minutes: " + str(minutes) + " minutes")
-print("Your age in hours: " + str(hours) + " hours")
-print("Your age in weeks: " + str(weeks) + " weeks")
-print("Your age in months: " + str(months) + " months")
+print('Hi, ' + str(name) + ', there is your results:')
+print('Your age in seconds: ' + str(seconds) + " seconds')
+print('Your age in minutes: ' + str(minutes) + " minutes')
+print('Your age in hours: ' + str(hours) + " hours')
+print('Your age in weeks: ' + str(weeks) + " weeks')
+print('Your age in months: '+ str(months) + ' months')
