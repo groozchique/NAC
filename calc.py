f6b19b9f3b356b23efa1ac27352751782d7a1f48
thegreatn <nikitos43rus@gmail.com>
2018-06-06 18:39:30 +0300

    Add missing integers and prevent cmd from closing

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 print('Welcome to Noob-Age-Calculator 1.0!')
 name  = input('Enter your name: ')
 age = int(input("Enter your age: '))
-if age < 1:
+if int(age) < 1 or int(age) > 100:
     print('You are not human!')
     quit()
 seconds = int(age) * 365 * 24 * 60 * 60
@@ -15,3 +15,4 @@ print('Your age in minutes: ' + str(minutes) + " minutes')
 print('Your age in hours: ' + str(hours) + " hours')
 print('Your age in weeks: ' + str(weeks) + " weeks')
 print('Your age in months: '+ str(months) + ' months')
+f = input('Press any button to close the window.')
