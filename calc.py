0b50186cbab20f2b6c16213cd8ae7125f2b78232
Nick <28346376+groozchique@users.noreply.github.com>
2020-02-10 19:29:58 +0300

    Simplify everything and bump version to 1.2

--- calc.py
+++ calc.py
@@ -1,16 +1,16 @@
-print('Welcome to N00b-Age-Calculator 1.1!')
+print('Welcome to N00b-Age-Calculator 1.2!')
 print('created by groozchique')
-age = int(input('Hello! Please, enter your age: '))
-while(int(age) < 1) or (int(age) > 110):
+a = int(input('Hello! Please, enter your age: '))
+while(int(a) < 1) or (int(a) > 110):
     print('Are you really human?')
-    age = int(input('Please, try again. Enter your age: '))
-seconds = int(age) * 365 * 24 * 60 * 60
-minutes = int(age) * 365 * 24 * 60
-hours = int(age) * 365 * 24
-weeks = int(age) * 12 * 4
-months = int(age) * 12
-ea = str(80 - int(age))
+    a = int(input('Please, try again. Enter your age: '))
+s = int(a) * 365 * 24 * 60 * 60
+m = int(a) * 365 * 24 * 60
+h = int(a) * 365 * 24
+w = int(a) * 12 * 4
+mn = int(a) * 12
+ea = str(80 - int(a))
 print('Ok, here is your results:')
-print('You survived in this cruel world for ' + str(seconds) + ' seconds' ', ' + str(minutes) + ' minutes' ', ' + str(hours) + ' hours' ', ' + str(weeks) + ' weeks' ' or ' + str(months) + ' months')
+print('You survived in this cruel world for ' + str(s) + ' seconds' ', ' + str(m) + ' minutes' ', ' + str(h) + ' hours' ', ' + str(w) + ' weeks' ' or ' + str(mn) + ' months')
 print('And you have to suffer in this world for, approximately, ' + str(ea) + ' years!')
 f = input('Press any button to close the window.')
